f12ad893f315cda1c010f24426e74eb529583fdd
50616046 <50616046@50616046-pc>
2017-11-06 17:38:19 +0900

    11/06_task_1

--- src/matrix/Matrix_main.java
+++ src/matrix/Matrix_main.java
@@ -8,11 +8,14 @@ public class Matrix_main {
 		// TODO Auto-generated method stub
 		double a[][] = {{0,1},{2,0},{0,3}};
 		double b[][] = {{1,-1,2},{3,0,4}};
-		Matrix_lib mlib = new Matrix_lib(a, b);
-		System.out.println("Product = "+Arrays.deepToString(mlib.getProduct()));
+		Matrix_lib mlib = new Matrix_lib();
+		System.out.println("Product = "+Arrays.deepToString(mlib.getProduct(a,b)));
 		System.out.println("Transpose = "+Arrays.deepToString(mlib.getTranspose(b)));
 		
+		double c[][] = {{3,1,1,2},{5,1,3,4},{2,0,1,0},{1,3,2,1}};
+		System.out.println("Cofactor = "+(mlib.getCofactor(c)));
 		
+		System.out.println("Inverse = "+Arrays.deepToString(mlib.getInverse(c)));
 	}
 
 }
